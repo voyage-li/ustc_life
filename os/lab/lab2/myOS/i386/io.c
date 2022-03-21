e69e1f0fa607eb2e76ab6a1836bf9454c06ae185
voyage <923959605@qq.com>
2022-03-21 23:59:50 +0800

    os lab2

---
+++ os/lab/lab2/myOS/i386/io.c
@@ -0,0 +1,21 @@
+#include "io.h"
+
+// inb，从端口号为 port_from 的端口读取一个字节
+unsigned char inb(unsigned short int port_from) {
+    unsigned char value;
+    __asm__ __volatile__ ("inb %w1, %b0": "=a"(value): "Nd"(port_from));
+    return value;
+}
+
+// outb，向端口号为 port_to 的端口输出一个字节
+void outb(unsigned short int port_to, unsigned char value) {
+    __asm__ __volatile__ ("outb %b0, %w1":: "a"(value), "Nd"(port_to));
+}
+
+int strlen(char *str)
+{
+    int len = 0;
+    while (*(str + len) != '\0')
+        len++;
+    return len;
+}
---
+++ os/lab/lab2/myOS/osStart.c
@@ -0,0 +1,17 @@
+#include "myPrintk.h"
+#include "vga.h"
+
+/* 此文件无需修改 */
+
+// 用户程序入口
+void myMain(void);
+
+void osStart(void)
+{
+    clear_screen();
+    myPrintk(0x2, "Starting the OS...\n");
+    myMain();
+    myPrintk(0x2, "Stop running... shutdown\n");
+    while (1)
+        ;
+}
