cb7982e632f9f441104f4af82262e1a63eaf547b
voyage <923959605@qq.com>
2023-03-15 14:03:39 +0800

    alg lab4

---
+++ forTA/7/cal.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char *p;
+    printf("%d", (int)(sizeof(p)));
+    return 0;
+}
--- tmp/tt.c
+++ tmp/tt.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
-#define N 1000000
-
 int main()
 {
-    int i, j, k, n;
-    int a[n];
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-
-    int sum[n];
-    for (j = 0; j < n - 2; j++)
-    {
-        sum[j] = a[j] + a[j + 1] + a[j + 2];
-    }
-
-    int min = sum[0];
-    for (k = 0; k < n - 2; k++)
-    {
-        if (sum[k] <= min)
-        {
-            min = sum[k];
-        }
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", k);
-    }
-    return 0;
+    char a[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    char *p = *a, (*q)[4] = a;
 }
