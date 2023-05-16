1990cea203147400f279976617f229ebb9c19f2c
voyage <923959605@qq.com>
2023-05-17 01:18:44 +0800

    AI LAB1 astar

--- AI/lab1/astar/src/run.sh
+++ AI/lab1/astar/src/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -v
 rm -rf ./astar ../output/*
-g++ astar.cpp -o astar -O3
+
+if [ $1 -eq 0 ]; then
+    g++ -DDEBUG astar.cpp -o astar -O3
+else
+    g++ astar.cpp -o astar -O3
+fi
+
 for i in {0..9}
 do
     ./astar <../input/input$i.txt >../output/output$i.txt
