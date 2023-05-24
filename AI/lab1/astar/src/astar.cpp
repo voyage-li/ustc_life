c4ecc52ddb0167262a6b38552dbc8e281fa40154
voyage <923959605@qq.com>
2023-05-25 00:52:02 +0800

    AI LAB1 差不多得了

--- AI/lab1/astar/src/astar.cpp
+++ AI/lab1/astar/src/astar.cpp
@@ -1,246 +1,54 @@
 #include <bits/stdc++.h>
-// #define DEBUG
 
-float calculate(std::vector<std::vector<int>> &data);
+#ifdef DEBUG
+std::string print(int num)
+{
+    std::string ans = "";
+    while (num != 0)
+    {
+        if (num % 2)
+            ans = "1" + ans;
+        else
+            ans = "0" + ans;
+        num = num / 2;
+    }
+    return ans;
+}
+#endif
+
+double h(std::vector<int> data);
+int reverse(std::vector<int> &data, int i, int j);
+int onlyReverse(std::vector<int> &data, int i, int j);
+int move(std::vector<int> &data, int i, int j, int s);
+std::vector<std::vector<int>> astar(std::vector<int> data);
 
 struct node
 {
     int i, j, s;
-    int f;
-    std::vector<std::vector<int>> data;
-    std::vector<std::vector<int>> visist;
+    int g;
+    std::vector<int> state;
+    std::vector<std::vector<int>> vis;
     friend bool operator<(struct node n1, struct node n2)
     {
-        if (n1.f + calculate(n1.data) == n2.f + calculate(n2.data))
-            return n1.f < n2.f;
-        else
-            return n1.f + calculate(n1.data) > n2.f + calculate(n2.data);
+        if (n1.g + h(n1.state) == n2.g + h(n2.state))
+            return n1.g < n2.g;
+        return n1.g + h(n1.state) > n2.g + h(n2.state);
     }
 };
-
-void reverse(std::vector<std::vector<int>> &data, int i, int j)
-{
-    int n = data.size();
-    if (i < 0 || j < 0 || i >= n || j >= n)
-        return;
-    data[i][j] = !data[i][j];
-}
-void Onlyreverse(std::vector<std::vector<int>> &data, int i, int j)
-{
-    int n = data.size();
-    if (i < 0 || j < 0 || i >= n || j >= n)
-        return;
-    if (data[i][j])
-        data[i][j] = !data[i][j];
-}
-
-int move(std::vector<std::vector<int>> &data, int i, int j, int s)
-{
-    int n = data.size();
-    if (s == 1)
-    {
-        if (j + 1 >= n || i - 1 < 0)
-            return -1;
-        reverse(data, i, j);
-        reverse(data, i, j + 1);
-        reverse(data, i - 1, j);
-    }
-    if (s == 2)
-    {
-        if (j - 1 < 0 || i - 1 < 0)
-            return -1;
-        reverse(data, i, j);
-        reverse(data, i - 1, j);
-        reverse(data, i, j - 1);
-    }
-    if (s == 3)
-    {
-        if (j - 1 < 0 || i + 1 >= n)
-            return -1;
-        reverse(data, i, j);
-        reverse(data, i, j - 1);
-        reverse(data, i + 1, j);
-    }
-    if (s == 4)
-    {
-        if (j + 1 >= n || i + 1 >= n)
-            return -1;
-        reverse(data, i, j);
-        reverse(data, i + 1, j);
-        reverse(data, i, j + 1);
-    }
-    return 0;
-}
-
-int cal1(std::vector<std::vector<int>> &data, int i, int j, int s, int flag)
-{
-    int n = data.size();
-    int ans = 0;
-    if (s == 1)
-    {
-        if (j + 1 >= n || i - 1 < 0)
-            return -1;
-        ans += data[i][j];
-        ans += data[i][j + 1];
-        ans += data[i - 1][j];
-        if (ans == flag)
-        {
-            Onlyreverse(data, i, j);
-            Onlyreverse(data, i, j + 1);
-            Onlyreverse(data, i - 1, j);
-        }
-    }
-    if (s == 2)
-    {
-        if (j - 1 < 0 || i - 1 < 0)
-            return -1;
-        ans += data[i][j];
-        ans += data[i - 1][j];
-        ans += data[i][j - 1];
-        if (ans == flag)
-        {
-            Onlyreverse(data, i, j);
-            Onlyreverse(data, i - 1, j);
-            Onlyreverse(data, i, j - 1);
-        }
-    }
-    if (s == 3)
-    {
-        if (j - 1 < 0 || i + 1 >= n)
-            return -1;
-        ans += data[i][j];
-        ans += data[i][j - 1];
-        ans += data[i + 1][j];
-        if (ans == flag)
-        {
-            Onlyreverse(data, i, j);
-            Onlyreverse(data, i, j - 1);
-            Onlyreverse(data, i + 1, j);
-        }
-    }
-    if (s == 4)
-    {
-        if (j + 1 >= n || i + 1 >= n)
-            return -1;
-        ans += data[i][j];
-        ans += data[i + 1][j];
-        ans += data[i][j + 1];
-        if (ans == flag)
-        {
-            Onlyreverse(data, i, j);
-            Onlyreverse(data, i, j + 1);
-            Onlyreverse(data, i + 1, j);
-        }
-    }
-    return ans;
-}
-
-float calculate(std::vector<std::vector<int>> &data)
-{
-    int n = data.size();
-    int cnt = 0;
-    std::vector<std::vector<int>> tmp = data;
-    int f = 3;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int p = 1; p <= 4; p++)
-                if (cal1(tmp, i, j, p, f) == f)
-                {
-                    cnt += 1;
-                    continue;
-                }
-
-    f = 2;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int p = 1; p <= 4; p++)
-                if (cal1(tmp, i, j, p, f) == f)
-                {
-                    cnt += 2;
-                    continue;
-                }
-    f = 1;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int p = 1; p <= 4; p++)
-                if (cal1(tmp, i, j, p, f) == f)
-                {
-                    cnt += 3;
-                    continue;
-                }
-
-    return cnt / 3.0;
-}
-
-std::vector<std::vector<int>> astar(std::vector<std::vector<int>> data)
-{
-    int n = data.size();
-    std::priority_queue<struct node> pro;
-    std::set<std::vector<std::vector<int>>> NSame;
-    std::vector<std::vector<int>> visit(n, std::vector<int>(n, 0));
-    struct node first;
-    first.data = data;
-    first.f = 0;
-    first.visist = visit;
-    pro.push(first);
-    while (!pro.empty())
-    {
-        std::vector<std::vector<int>> now = pro.top().data;
-        std::vector<std::vector<int>> vis = pro.top().visist;
-        int fmp = pro.top().f;
-        pro.pop();
-#ifdef DEBUG
-        std::cout << fmp << "+" << calculate(now) << "=" << fmp + calculate(now) << std::endl;
-#endif
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                for (int p = 1; p <= 4; p++)
-                {
-                    if (move(now, i, j, p) == -1)
-                        continue;
-                    struct node tmp;
-                    tmp.i = i;
-                    tmp.j = j;
-                    tmp.s = p;
-                    tmp.f = fmp + 1;
-                    tmp.data = now;
-                    tmp.visist = vis;
-                    tmp.visist[i][j] ^= 1 << (p - 1);
-                    if (calculate(tmp.data) == 0)
-                        return tmp.visist;
-                    if (!NSame.count(tmp.data))
-                        pro.push(tmp);
-                    move(now, i, j, p);
-                }
-            }
-        }
-    }
-    return visit;
-}
 
 int main()
 {
     int n;
     std::cin >> n;
-    std::vector<std::vector<int>> data(n, std::vector<int>(n));
+    std::vector<int> data(n, 0);
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            std::cin >> data[i][j];
-
-    // int t;
-    // std::cin >> t;
-    // while (t--)
-    // {
-    //     int i, j, s;
-    //     scanf("%d,%d,%d", &i, &j, &s);
-    //     move(data, i, j, s);
-    // }
-    // for (int i = 0; i < n; i++)
-    //     for (int j = 0; j < n; j++)
-    //         std::cout << data[i][j];
-    // return 0;
+        {
+            int tmp;
+            std::cin >> tmp;
+            if (tmp == 1)
+                data[i] = data[i] ^ (1 << (n - 1 - j));
+        }
     auto visit = astar(data);
 
     int num = 0;
@@ -260,4 +68,238 @@ int main()
                     std::cout << i << "," << j << "," << p + 1 << std::endl;
 
     return 0;
+}
+
+int cal(std::vector<int> &data, int i, int j, int s)
+{
+    int n = data.size();
+    int ans = 0;
+    if (s == 1)
+    {
+        if (j + 1 >= n || i - 1 < 0)
+            return -1;
+        ans += onlyReverse(data, i, j);
+        ans += onlyReverse(data, i, j + 1);
+        ans += onlyReverse(data, i - 1, j);
+    }
+    if (s == 2)
+    {
+        if (j - 1 < 0 || i - 1 < 0)
+            return -1;
+        ans += onlyReverse(data, i, j);
+        ans += onlyReverse(data, i - 1, j);
+        ans += onlyReverse(data, i, j - 1);
+    }
+    if (s == 3)
+    {
+        if (j - 1 < 0 || i + 1 >= n)
+            return -1;
+        ans += onlyReverse(data, i, j);
+        ans += onlyReverse(data, i, j - 1);
+        ans += onlyReverse(data, i + 1, j);
+    }
+    if (s == 4)
+    {
+        if (j + 1 >= n || i + 1 >= n)
+            return -1;
+        ans += onlyReverse(data, i, j);
+        ans += onlyReverse(data, i + 1, j);
+        ans += onlyReverse(data, i, j + 1);
+    }
+    return ans;
+}
+
+double h1(std::vector<int> state)
+{
+    int n = state.size();
+    int cnt1 = 0, cnt = 0;
+    std::vector<int> data = state;
+    int f = 3;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int p = 1; p <= 4; p++)
+            {
+                if (move(data, i, j, p) == f)
+                {
+                    cal(data, i, j, p);
+                    cnt += 1;
+                }
+                else
+                    move(data, i, j, p);
+            }
+    f = 2;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int p = 1; p <= 4; p++)
+            {
+                if (move(data, i, j, p) == f)
+                {
+                    cal(data, i, j, p);
+                    cnt += 2;
+                }
+                else
+                    move(data, i, j, p);
+            }
+    f = 1;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int p = 1; p <= 4; p++)
+            {
+                if (move(data, i, j, p) == f)
+                {
+                    cal(data, i, j, p);
+                    cnt += 3;
+                }
+                else
+                    move(data, i, j, p);
+            }
+    cnt1 = cnt;
+    // cnt = 0;
+    return cnt / 3.0;
+}
+
+double h(std::vector<int> state)
+{
+    std::vector<int> data = state;
+    int n = data.size();
+    int retValue = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (((data[i] >> (n - 1 - j)) & 1) == 0)
+                continue;
+            int cnt = 0;
+            std::queue<std::pair<int, int>> q;
+            q.push(std::make_pair(i, j));
+            while (!q.empty())
+            {
+                auto now = q.front();
+                q.pop();
+                for (int ii = now.first - 1; ii <= now.first + 1; ii++)
+                    for (int jj = now.second - 1; jj <= now.second + 1; jj++)
+                        if (onlyReverse(data, ii, jj) == 1)
+                            q.push(std::make_pair(ii, jj)), cnt++;
+            }
+            retValue += (cnt % 3) ? (cnt / 3 + 1) : (cnt / 3);
+        }
+    }
+    return retValue * 1.5;
+}
+
+int onlyReverse(std::vector<int> &data, int i, int j)
+{
+    int n = data.size();
+    if (i < 0 || j < 0 || i >= n || j >= n)
+        return -1;
+    int tmp = data[i];
+    data[i] ^= (1 << (n - 1 - j));
+    if (tmp > data[i])
+        return 1;
+    else
+    {
+        data[i] = tmp;
+        return 0;
+    }
+}
+
+int reverse(std::vector<int> &data, int i, int j)
+{
+    int n = data.size();
+    if (i < 0 || j < 0 || i >= n || j >= n)
+        return -1;
+    int tmp = data[i];
+    data[i] ^= (1 << (n - 1 - j));
+    if (tmp > data[i])
+        return 1;
+    else
+        return 0;
+}
+
+int move(std::vector<int> &data, int i, int j, int s)
+{
+    int n = data.size();
+    int ans = 0;
+    if (s == 1)
+    {
+        if (j + 1 >= n || i - 1 < 0)
+            return -1;
+        ans += reverse(data, i, j);
+        ans += reverse(data, i, j + 1);
+        ans += reverse(data, i - 1, j);
+    }
+    if (s == 2)
+    {
+        if (j - 1 < 0 || i - 1 < 0)
+            return -1;
+        ans += reverse(data, i, j);
+        ans += reverse(data, i - 1, j);
+        ans += reverse(data, i, j - 1);
+    }
+    if (s == 3)
+    {
+        if (j - 1 < 0 || i + 1 >= n)
+            return -1;
+        ans += reverse(data, i, j);
+        ans += reverse(data, i, j - 1);
+        ans += reverse(data, i + 1, j);
+    }
+    if (s == 4)
+    {
+        if (j + 1 >= n || i + 1 >= n)
+            return -1;
+        ans += reverse(data, i, j);
+        ans += reverse(data, i + 1, j);
+        ans += reverse(data, i, j + 1);
+    }
+    return ans;
+}
+
+std::vector<std::vector<int>> astar(std::vector<int> data)
+{
+    int n = data.size();
+    std::priority_queue<struct node> fque;
+    std::set<std::vector<int>> NSame;
+    std::vector<std::vector<int>> visit(n, std::vector<int>(n, 0));
+    struct node firststate = {0, 0, 0, 0, data, visit};
+    fque.push(firststate);
+    while (!fque.empty())
+    {
+        struct node tmp = fque.top();
+        fque.pop();
+#ifdef DEBUG
+        std::cout << tmp.g << "+" << h(tmp.state) << "=" << tmp.g + h(tmp.state) << std::endl;
+#endif
+        std::priority_queue<struct node> tque;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int p = 1; p <= 4; p++)
+                {
+                    if (move(tmp.state, i, j, p) == -1)
+                        continue;
+                    if (h(tmp.state) == 0)
+                    {
+                        tmp.vis[i][j] ^= 1 << (p - 1);
+                        return tmp.vis;
+                    }
+                    struct node next = {i, j, p, tmp.g + 1, tmp.state, tmp.vis};
+                    next.vis[i][j] ^= 1 << (p - 1);
+                    if (!NSame.count(next.state))
+                    {
+                        tque.push(next);
+                        NSame.insert(next.state);
+                    }
+                    move(tmp.state, i, j, p);
+                }
+            }
+        }
+        for (int i = 0; !tque.empty(); i++)
+        {
+            fque.push(tque.top());
+            tque.pop();
+        }
+    }
+    return visit;
 }
