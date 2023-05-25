a3638f59d48112ab52841c94fe815470d7c6de67
voyage <923959605@qq.com>
2023-05-25 23:03:32 +0800

    AI LAB1

--- AI/lab1/astar/src/astar.cpp
+++ AI/lab1/astar/src/astar.cpp
@@ -26,13 +26,13 @@ struct node
 {
     int i, j, s;
     int g;
-    std::vector<int> state;
+    double hv;
     std::queue<std::tuple<int, int, int>> vis;
     friend bool operator<(struct node n1, struct node n2)
     {
-        if (n1.g + h(n1.state) == n2.g + h(n2.state))
+        if (n1.g + n1.hv == n2.g + n2.hv)
             return n1.g < n2.g;
-        return n1.g + h(n1.state) > n2.g + h(n2.state);
+        return n1.g + n1.hv > n2.g + n2.hv;
     }
 };
 
@@ -65,94 +65,6 @@ int main()
     return 0;
 }
 
-int cal(std::vector<int> &data, int i, int j, int s)
-{
-    int n = data.size();
-    int ans = 0;
-    if (s == 1)
-    {
-        if (j + 1 >= n || i - 1 < 0)
-            return -1;
-        ans += onlyReverse(data, i, j);
-        ans += onlyReverse(data, i, j + 1);
-        ans += onlyReverse(data, i - 1, j);
-    }
-    if (s == 2)
-    {
-        if (j - 1 < 0 || i - 1 < 0)
-            return -1;
-        ans += onlyReverse(data, i, j);
-        ans += onlyReverse(data, i - 1, j);
-        ans += onlyReverse(data, i, j - 1);
-    }
-    if (s == 3)
-    {
-        if (j - 1 < 0 || i + 1 >= n)
-            return -1;
-        ans += onlyReverse(data, i, j);
-        ans += onlyReverse(data, i, j - 1);
-        ans += onlyReverse(data, i + 1, j);
-    }
-    if (s == 4)
-    {
-        if (j + 1 >= n || i + 1 >= n)
-            return -1;
-        ans += onlyReverse(data, i, j);
-        ans += onlyReverse(data, i + 1, j);
-        ans += onlyReverse(data, i, j + 1);
-    }
-    return ans;
-}
-
-double h1(std::vector<int> state)
-{
-    int n = state.size();
-    int cnt1 = 0, cnt = 0;
-    std::vector<int> data = state;
-    int f = 3;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int p = 1; p <= 4; p++)
-            {
-                if (move(data, i, j, p) == f)
-                {
-                    cal(data, i, j, p);
-                    cnt += 1;
-                }
-                else
-                    move(data, i, j, p);
-            }
-    f = 2;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int p = 1; p <= 4; p++)
-            {
-                if (move(data, i, j, p) == f)
-                {
-                    cal(data, i, j, p);
-                    cnt += 2;
-                }
-                else
-                    move(data, i, j, p);
-            }
-    f = 1;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int p = 1; p <= 4; p++)
-            {
-                if (move(data, i, j, p) == f)
-                {
-                    cal(data, i, j, p);
-                    cnt += 3;
-                }
-                else
-                    move(data, i, j, p);
-            }
-    cnt1 = cnt;
-    // cnt = 0;
-    return cnt / 3.0;
-}
-
 double h(std::vector<int> state)
 {
     std::vector<int> data = state;
@@ -176,10 +88,12 @@ double h(std::vector<int> state)
                         if (onlyReverse(data, ii, jj) == 1)
                             q.push(std::make_pair(ii, jj)), cnt++;
             }
-            retValue += (cnt % 3) ? (cnt / 3 + 1) : (cnt / 3);
+            int tmp = (cnt % 3) ? (cnt / 3 + 1) : (cnt / 3);
+            tmp += (cnt % 2) ? (!(tmp % 2)) : (tmp % 2);
+            retValue += tmp;
         }
     }
-    return retValue * 1.5;
+    return retValue;
 }
 
 int onlyReverse(std::vector<int> &data, int i, int j)
@@ -214,6 +128,8 @@ int reverse(std::vector<int> &data, int i, int j)
 int move(std::vector<int> &data, int i, int j, int s)
 {
     int n = data.size();
+    if (i < 0 || i >= n || j < 0 || j >= n)
+        return -1;
     int ans = 0;
     if (s == 1)
     {
@@ -250,51 +166,67 @@ int move(std::vector<int> &data, int i, int j, int s)
     return ans;
 }
 
+void judge(std::vector<int> &data, int i, int j, int s, int g, std::queue<std::tuple<int, int, int>> &vis, std::priority_queue<struct node> &fque)
+{
+    if (move(data, i, j, s) == -1)
+        return;
+    struct node next = {i, j, s, g + 1, h(data), vis};
+    next.vis.push(std::make_tuple(i, j, s));
+    fque.push(next);
+    move(data, i, j, s);
+}
+
 std::queue<std::tuple<int, int, int>> astar(std::vector<int> data)
 {
     int n = data.size();
     std::priority_queue<struct node> fque;
-    std::set<std::vector<int>> NSame;
     std::queue<std::tuple<int, int, int>> visit;
-    struct node firststate = {0, 0, 0, 0, data, visit};
+    struct node firststate = {0, 0, 0, 0, h(data), visit};
     fque.push(firststate);
     while (!fque.empty())
     {
         struct node tmp = fque.top();
         fque.pop();
+        auto road = tmp.vis;
+        auto now = data;
+        while (!road.empty())
+        {
+            auto movement = road.front();
+            road.pop();
+            move(now, std::get<0>(movement), std::get<1>(movement), std::get<2>(movement));
+        }
 #ifdef DEBUG
-        std::cout << tmp.g << "+" << h(tmp.state) << "=" << tmp.g + h(tmp.state) << std::endl;
+        std::cout << tmp.g << "+" << tmp.hv << "=" << tmp.g + tmp.hv << std::endl;
 #endif
-        std::priority_queue<struct node> tque;
+        int tmpi = -1, tmpj = -1;
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                for (int p = 1; p <= 4; p++)
+                if (((now[i] >> (n - 1 - j)) & 1))
                 {
-                    if (move(tmp.state, i, j, p) == -1)
-                        continue;
-                    if (h(tmp.state) == 0)
-                    {
-                        tmp.vis.push(std::make_tuple(i, j, p));
-                        return tmp.vis;
-                    }
-                    struct node next = {i, j, p, tmp.g + 1, tmp.state, tmp.vis};
-                    next.vis.push(std::make_tuple(i, j, p));
-                    if (!NSame.count(next.state))
-                    {
-                        tque.push(next);
-                        NSame.insert(next.state);
-                    }
-                    move(tmp.state, i, j, p);
+                    tmpi = i;
+                    tmpj = j;
+                    break;
                 }
             }
+            if (tmpi != -1)
+                break;
         }
-        for (int i = 0; !tque.empty(); i++)
-        {
-            fque.push(tque.top());
-            tque.pop();
-        }
+        if (tmpi == -1 && tmpj == -1)
+            return tmp.vis;
+        judge(now, tmpi - 1, tmpj, 3, tmp.g, tmp.vis, fque);
+        judge(now, tmpi - 1, tmpj, 4, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj + 1, 2, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj + 1, 3, tmp.g, tmp.vis, fque);
+        judge(now, tmpi + 1, tmpj, 1, tmp.g, tmp.vis, fque);
+        judge(now, tmpi + 1, tmpj, 2, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj - 1, 1, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj - 1, 4, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj, 1, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj, 2, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj, 3, tmp.g, tmp.vis, fque);
+        judge(now, tmpi, tmpj, 4, tmp.g, tmp.vis, fque);
     }
     return visit;
 }
